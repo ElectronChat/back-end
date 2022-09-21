182fb02fde0a9a9ed1b0b97bffd42d36515ab3bf
james <slumburger15@gmail.com>
2022-09-21 13:19:24 -0700

    testagain

--- hello-world.js
+++ hello-world.js
@@ -5,11 +5,7 @@ const route = require('./routes');
 const app = express();
 const hostname = '69.48.142.114';
 const port = 80;
-app.use(bodyParser.urlencoded({extended: false}));
-app.use('/home',route);
-app.use((req, res,next)=>{
-   res.sendFile('../front-end/electron-chat/dist/electron-chat/index.html', {root: __dirname});
-});
+app.get('/', (req, res) => { res.sendFile('../' + __dirname + 'front-end/electron-chat/dist/electron-chat/index.html'); });
 const server = http.createServer(app);
 
 server.listen(port, hostname, () => {
