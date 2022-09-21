4b50086ad8e677b5c60fd95da1f43f5a3e7ba83e
James-R-Marshall <61856546+James-R-Marshall@users.noreply.github.com>
2022-09-21 12:34:47 -0700

    Update hello-world.js

--- hello-world.js
+++ hello-world.js
@@ -1,13 +1,16 @@
 const http = require('http');
-
+const express = require('express');
+const bodyParser = require('body-parser');
+const route = require('./routes');
+const app = express();
 const hostname = '69.48.142.114';
 const port = 80;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use('/test',route);
+app.use((req, res,next)=>{
+   res.status(404).send('<h1> Page not found </h1>');
 });
+const server = http.createServer(app);
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
