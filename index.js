c0e71be577e55f9afe89c86ff06602fcb48d178c
james <slumburger15@gmail.com>
2022-09-26 13:02:05 -0700

    more testing

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require( 'express' );
 const http = require('http');
 const app = express()
 var distDir = __dirname + "/front/";
-app.use('/', express.static('/home/back-end/front/index.html'));
-// app.get('/', (req,res)=>
-// {
-//     res.sendFile('/home/front-end/frontend/dist/frontend/index.html');
-// });
+app.use(express.static('/home/back-end/front'));
+
+app.get('/', (req,res)=>
+{
+     res.sendFile('/home/front-end/frontend/dist/frontend/index.html');
+});
 
 const hostname = '69.48.142.114';
 const port = 80;
