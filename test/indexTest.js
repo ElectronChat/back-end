5c034a6aab3d881361cc86323968b1c253882d40
Zack Khalidov <46911135+zkhalido@users.noreply.github.com>
2022-09-27 21:17:08 -0600

    Revert "adding mocha and chai dependencies. setup test test" This reverts commit 6689c41cbecee72be7d5c37f28d5b09119033d56.

--- test/indexTest.js
+++
@@ -1,9 +0,0 @@
-const assert = require('chai').assert;
-const index = require('../index');
-
-//test test
-describe('Index ', function(){
-    it('app should return hello', function() {
-        assert.equal(index(), 'hello');
-    })
-});
