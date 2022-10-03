cc7730544fa63b59db3ac98e840d22d5eae8e801
Zack Khalidov <46911135+zkhalido@users.noreply.github.com>
2022-10-02 21:19:35 -0600

    Update index.js
    testing index.js files without IP

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
-let express = require( 'express' );
-let http = require('http');
+let express = require('express');
+let app = express();
 
 // we need to include any of our created routes so it will be 'linked'
-const home = require('./app/routes/main')
+const home = require( './app/routes/main');
 
 // disabling this because sonar said it was a security threat
 let app1 = express();  // Compliant
 app1.disable("x-powered-by");
 
-// we create our app and give it some middleware
-let app = express();
+// middleware function 
+app.get('/', function (req, res, next) {
+  next();
+})
 
 // this is our directory where all of our frontend gets built to. 
 var distDir = __dirname + "/front/";
-app.use(express.static('/home/back-end/front'));
+app.use('/home/back-end/front');
 
 // here is our routing pipeline, we reference each of our routing modules that we need right here.
 // the first route is how we will start creating our routes.
 app.use('/', home);
 
-const hostname = '69.48.142.114';
 const port = 80;
-app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port, () => {
+  console.log(err, app.address());
 });
-//test function for mocha test
+
+// test function for mocha test
 module.exports = function() {
   return 'hello';
 }
